928c7d12e96a1c45ff5872da431beeda9145362a
Dace Spulga Osina <dacespulgaosina@gmail.com>
2023-12-26 17:35:36 +0200

    alertcard modified

--- AlertCard.tsx
+++ AlertCard.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState} from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 
-const NoteCard = ({ note }) => {
+const AlertCard = ({ note }) => {
+    const [decimalValue, setDecimalValue] = useState('');
+
+
+    const handleSend = () => {
+        // Handle the logic for sending the decimal value
+        console.log('Decimal Value:', decimalValue);
+        // You can perform actions here like sending the decimalValue to an API or performing a specific action
+      };
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{note.Title}</Text>
+
+      <View style={styles.inputContainer}>
+        {/* Input field for decimal number */}
+        <TextInput
+          style={styles.decimalInput}
+          placeholder="Enter decimal number"
+          value={decimalValue}
+          onChangeText={setDecimalValue}
+          keyboardType="decimal-pad"
+        />
+        
+        {/* Send button */}
+        <TouchableOpacity style={styles.sendButton} onPress={handleSend}>
+          <Text style={styles.sendButtonText}>Send</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -35,6 +61,29 @@ const styles = StyleSheet.create({
     color: 'blue',
     marginTop: 8,
   },
+  inputContainer: {
+    flexDirection: 'row', // Horizontal layout
+    alignItems: 'center',
+},
+
+  decimalInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    marginBottom: 8,
+  },
+  sendButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  sendButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
 });
 
-export default NoteCard;
+export default AlertCard;
